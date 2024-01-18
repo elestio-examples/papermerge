c95845955de35e79e45647b7504ec225c531f4a9
Prem chavhan <pchavhan@logiquad.com>
2024-01-18 16:33:30 +0530

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cp -rf ./papermerge/* ./
-rm docker-compose.yml
+rm docker-compose.yaml
 mv docker-compose-new.yml docker-compose.yml
 docker buildx build . --output type=docker,name=elestio4test/papermerge:latest | docker load
