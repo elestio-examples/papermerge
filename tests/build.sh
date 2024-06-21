f746589f52440e4792bfd38a179efbdc7527324a
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-06-21 12:58:20 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-mv ./docker/prod/* ./
-sed -i "s~docker/prod/~~g" ./Dockerfile
+cp ./papermerge/Dockerfile ./Dockerfile
 mv ./README-new.md ./README.md
 
 docker buildx build . --output type=docker,name=elestio4test/papermerge:latest | docker load
